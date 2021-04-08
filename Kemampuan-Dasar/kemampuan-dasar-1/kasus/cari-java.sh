37d1c166da117ff0f995169912b0421274ccebc9
supriansyahfaisal <supriansyahfaisal@gmail.com>
2021-04-09 04:21:26 +0700

    Pretest

---
+++ Kemampuan-Dasar/kemampuan-dasar-1/kasus/cari-java.sh
@@ -0,0 +1,11 @@
+printf "\n Please enter the path to check "
+read path2check
+file="*.java"
+
+if find $path2check -name "*.java" -print -quit |
+  grep -q '^'; then 
+ echo "ada file java di directory $path2check"
+else
+ echo "tidak ada file java di directory $path2check"
+fi
+
---
+++ Kemampuan-Dasar/kemampuan-dasar-1/latihan/powershellscripts/Sum.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Add two numeric value
+((sum=4+2))
+
+#Print the result
+echo $sum
