c403040519bcfc278da6dcae1d2a7aaffd76da4b
supriansyahfaisal <supriansyahfaisal@gmail.com>
2021-05-26 23:52:39 +0700

    projek Kotlin REST-API

---
+++ Kurikulum/interprise-Backend/Kotlin-RestAPI/KotlinRestAPI/src/test/kotlin/PraxisAcademy/KotlinRestAPI/KotlinRestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package PraxisAcademy.KotlinRestAPI
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinRestApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
