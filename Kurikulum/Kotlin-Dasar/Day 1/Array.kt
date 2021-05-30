54e3e1a5a0232fb5c22bb756b64587b700d22799
FaisalSupriansyah <supriansyahfaisal@gmail.com>
2021-05-31 04:53:48 +0700

    kotlin Day 2

---
+++ Kurikulum/Kotlin-Dasar/Day 1/Array.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val members: Array<String> = arrayOf("Anjing","Susah","Bangsat")
+    println(members[0])
+
+
+
+    // size : untuk mendapatkan panjang array
+    // get(index) : Mendapatkan data di posisi index
+    //[index] : Mendapatkan data posisi index
+    //set(index,value) : mengubah data diposisi index
+    //[index] = value : Mengubah data diposisi in9dex
+}
