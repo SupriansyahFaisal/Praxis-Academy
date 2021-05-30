607f02f001340713c4ec1f1b785478a0eefc93f2
FaisalSupriansyah <supriansyahfaisal@gmail.com>
2021-05-30 14:39:08 +0700

    Kotlin Day 1

---
+++ Kurikulum/Kotlin-Dasar/Day 1/DataString.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var firstname : String = "Muhammad Faisal "
+    var lastname : String = "Supriansyah"
+    var fullname : String = "$firstname $lastname"
+
+    var address : String = """
+        >Kepulauan Riau,Tanjung Pinang,
+        >Jalan,Pantai impian,Gang.udang,No.56
+        """.trimMargin(">")
+
+    println(firstname+lastname)
+    println(address)
+    var temp : String = "$fullname \nPanjang = ${fullname.length}"
+    print(temp)
+
+}
