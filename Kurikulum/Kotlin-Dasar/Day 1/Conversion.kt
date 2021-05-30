6722c290bf115debd879a28f709334640d02b138
supriansyahfaisal <supriansyahfaisal@gmail.com>
2021-05-30 14:33:04 +0700

    kotlin Day 1

---
+++ Kurikulum/Kotlin-Dasar/Day 1/Conversion.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var Number: Long = 9_000_000_000L
+    println(Number)
+    println("Conversion")
+    var NumberInt:Int = Number.toInt()
+    println(NumberInt)
+}
---
+++ Kurikulum/Kotlin-Dasar/Day 1/DataCharacter.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var f: Char= 'f'
+    var s: Char= 's'
+    var l: Char= 'l'
+
+    print(f)
+    print(s)
+    print(l)
+}
