e7d11b368119f413f9d767deb841297a77231f7a
FaisalSupriansyah <supriansyahfaisal@gmail.com>
2021-05-30 14:44:17 +0700

    Kotlin Day 1

---
+++ Kurikulum/Kotlin-Dasar/Day 1/HelloWorld.kt
@@ -0,0 +1,6 @@
+fun String() {
+    println("Hello World")
+    println("Hello Faisal")
+    println("Muhammad Faisal supriansyah")
+    println("Praxis-Academy April 2021")
+}
---
+++ Kurikulum/Kotlin-Dasar/Day 1/IntergerNumber.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var age: Byte=30
+    var Height: Int=170
+    var distance: Short = 2000
+    var balance: Long = 1000000000L
+
+    println("Byte = "+ age)
+    println("Int = "+Height)
+    println("Short = "+distance)
+    println("Long = "+balance)
+
+
+}
