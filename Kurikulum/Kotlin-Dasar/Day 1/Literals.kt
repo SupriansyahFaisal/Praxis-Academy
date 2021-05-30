a6df4184eccd6076c7d019d54384f52736f87cdd
FaisalSupriansyah <supriansyahfaisal@gmail.com>
2021-05-30 21:13:05 +0700

    Kotlin Day-1

---
+++ Kurikulum/Kotlin-Dasar/Day 1/Literals.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var DecimalLiteral : Int = 300
+    var HexadecimalLiteral : Int = 0xFF
+    var binariLiteral : Int = 0b1000
+
+    println(DecimalLiteral)
+    println(HexadecimalLiteral)
+    println(binariLiteral)
+}
