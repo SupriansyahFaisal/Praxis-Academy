214246b941c2ca6284a57f5fc5fdc233d474e91c
FaisalSupriansyah <supriansyahfaisal@gmail.com>
2021-05-31 05:01:16 +0700

    kotlin Day 2

--- Kurikulum/Kotlin-Dasar/Day 1/Array.kt
+++ Kurikulum/Kotlin-Dasar/Day 1/Array.kt
@@ -1,8 +1,9 @@
 fun main() {
-    val members: Array<String> = arrayOf("Anjing","Susah","Bangsat")
+    val members: Array<String> = arrayOf("Muhammad Faisal supriansyah","Ryan Ardiansyah","Eko Dharmansyah Susanto")
+
     println(members[0])
-
-
+    println(members[1])
+    println(members[2])
 
     // size : untuk mendapatkan panjang array
     // get(index) : Mendapatkan data di posisi index
