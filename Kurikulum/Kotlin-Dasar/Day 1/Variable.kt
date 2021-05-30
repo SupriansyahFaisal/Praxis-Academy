e4ce6b26a785cb60084708e99f9e9f155615f42b
FaisalSupriansyah <supriansyahfaisal@gmail.com>
2021-05-31 04:54:41 +0700

    kotlin Day 2

---
+++ Kurikulum/Kotlin-Dasar/Day 1/Variable.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var FirstName : String? = null
+    val age = 30
+
+    println(FirstName?.length)
+    println(age)
+    FirstName = "Muhammad Faisal supriansyah"
+    println(FirstName?.length)
+}
